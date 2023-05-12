9a3ebfaf44130d43d6ed5557a0534ff42114538a
SenhorArroz <luizrob.guimaraes@hotmail.com>
2023-05-12 10:12:01 -0300

    é a vida

--- app/database/tables.sql
+++ app/database/tables.sql
@@ -1,4 +1,5 @@
-DROP TABLE usuarios;
+DROP TABLE IF EXISTS usuarios;
+
 CREATE TABLE IF NOT EXISTS usuarios (
     id              INTEGER PRIMARY KEY,
     nome            TEXT    NOT NULL,
@@ -7,13 +8,6 @@ CREATE TABLE IF NOT EXISTS usuarios (
     ativado         INTEGER
 );
 
-INSERT INTO usuarios (id, nome, dataNascimento, tipo, ativado) values (1,'teste','01-01-2000',1,1);
-
-DROP TABLE veiculos;
-CREATE TABLE IF NOT EXISTS veiculos (
-    id              INTEGER PRIMARY KEY,
-    placa           TEXT    NOT NULL,
-    modelo          TEXT,
-    cor             TEXT,
-    ano             INTEGER
-);
+INSERT INTO usuarios (id, nome, dataNascimento, tipo, ativado) values (1,'teste 1','01-01-2000',1,1);
+INSERT INTO usuarios (id, nome, dataNascimento, tipo, ativado) values (2,'teste 2','01-01-2001',1,1);
+INSERT INTO usuarios (id, nome, dataNascimento, tipo, ativado) values (3,'teste 3','01-01-2003',1,1);
